9ea600d711b7acb5a9e529ca33384eb342b5f563
강연진 <yeonjin917n.n@gmail.com>
2024-05-29 21:28:53 +0900

    0529_AM_commit_splitTest

---
+++ chap10-api-lecture-source/src/main/java/com/ohgiraffers/api/split/slitTest_add.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.api.split;
+
+public class slitTest_add {
+    //공백을 문자로 바꿔준다
+    //동일글자를 제외한 숫자를센다
+
+
+}
--- chap10-api-lecture-source/src/main/java/com/ohgiraffers/api/split/splitTest.java
+++ chap10-api-lecture-source/src/main/java/com/ohgiraffers/api/split/splitTest.java
@@ -1,11 +1,23 @@
 package com.ohgiraffers.api.split;
 
 public class splitTest {
-//1. `"J a v a P r o g r a m "` 문자열 값을 공백 단위로 분리하 `char[]` 에 저장하여 출력하시오.
-//2. 분리전 글자 및 개수 출력
-//3. 분리 후 문자배열 갯수 출력 확인
-//4. `char[]` 값 출력
-//5. `char[]` 을 다시 `String` 으로 변환, 모두 대문자로 변환하여 출력
 
+    public static void main(String[] args) {
+        String str = "J a v a P r o g r a m ";
+
+//        System.out.println(str.replaceAll(" "," ").length());  //공백 포함 글자수
+//        System.out.println(str.replaceAll(" ", "").length());  //공백제외 글자수
+
+        String[] words = str.split(" "); //공백
+        char[] cha = new char[words.length];  //문자열을 배열로
+
+        System.out.println(cha);
+
+        //모두 대문자 출력
+        String upperCase = str.toUpperCase();
+        System.out.println(upperCase);
+
+
+}
 
 }
